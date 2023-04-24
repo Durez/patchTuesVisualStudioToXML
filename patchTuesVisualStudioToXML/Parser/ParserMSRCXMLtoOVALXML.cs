9db6529d1074fbc79552d17cdacb6f81b6f32b41
George A <totalwor156@gmail.com>
2023-04-24 10:50:31 +0300

    add main method

---
+++ patchTuesVisualStudioToXML/Parser/ParserMSRCXMLtoOVALXML.cs
@@ -0,0 +1,28 @@
+﻿using patchTuesVisualStudioToXML.GeneratorXML;
+using patchTuesVisualStudioToXML.Parser.models.cvrfXMLmodel;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace patchTuesVisualStudioToXML.Parser
+{
+    public class ParserMSRCXMLtoOVALXML
+    {
+        public void GetProductData(string productname = "Visual Studio") 
+        { 
+            MSrcAPIController controller =  new MSrcAPIController();
+            DateTime timestart = DateTime.Now;
+            Console.WriteLine("Start using Microsoft API");
+            Cvrfdoc doc =  controller.GETXMLCvrfAsync(controller.GETRawCvrfURLAsync().GetAwaiter().GetResult()).GetAwaiter().GetResult();
+            string duration = (DateTime.Now - timestart).TotalSeconds.ToString("0.##");
+            Console.WriteLine("Create CVRFXML Document is done. Duration: " + duration + " seconds");
+
+            new GeneratorXMLData().GenerateXMLData(doc);
+
+
+
+
+            Console.WriteLine("Writing OVAL XML to file complited.");
+        }
+    }
+}
