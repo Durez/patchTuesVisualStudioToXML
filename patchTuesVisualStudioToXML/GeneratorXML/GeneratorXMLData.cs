8bb487f4b4b6e72ad9da246e0d4254c95de4ec29
George A <totalwor156@gmail.com>
2023-04-24 10:48:20 +0300

    add creation of referense tags add random ids

--- patchTuesVisualStudioToXML/GeneratorXML/GeneratorXMLData.cs
+++ patchTuesVisualStudioToXML/GeneratorXML/GeneratorXMLData.cs
@@ -10,28 +10,39 @@ namespace patchTuesVisualStudioToXML.GeneratorXML
 {
     public class GeneratorXMLData
     {
-        const string schemaVersion = "5.11";
+        private const string schemaVersion = "5.11";
         private Cvrfdoc cvrfdoc {get;}
         private TagsGeneratorOVALXML generatorTags { get; }
+        public Oval_definitions resultOVALXML { get; set; }
         public GeneratorXMLData()
         {
             generatorTags = new TagsGeneratorOVALXML();
+            resultOVALXML = generatorTags.LoadSample();
         }
         public void GenerateXMLData(Cvrfdoc cvrfdoc)
         {
-            int lastDefID = 12345;
-            int lastTestID = 23456;
+            int lastDefID = 11111;
+            int lastTestID = 22222;
+            int lastDefrefID = 77777;
+            int lastObjID = 55555;
+            int lastStateID = 33333;
+            int lastVarID = 44444;
 
-            Oval_definitions resultOVALXML = generatorTags.LoadSample();
+            
 
-            resultOVALXML.generator = generatorTags.GenerateTagGenerator(schemaVersion, "OVALXMLgen by AGA");
+            resultOVALXML.generator = new GeneratorTAG(schemaVersion, "OVALXMLgen by AGA");
 
             List<Vulnerability> VulnerabListFiltredByProj = GetVulnerabilities(cvrfdoc); //TODO LINQ
+
+            resultOVALXML.tests.registryTest = new List<RegistryTest>();
 
             //tag defenitions
             resultOVALXML.definitions.definitionsList = new List<Definition>();
             foreach (Vulnerability vulnerability in VulnerabListFiltredByProj)
             {
+                //list of ref docs (inventory)
+                List<Definition> refInventoryDef = new List<Definition>();
+
                 //root of def
                 Definition definition = new Definition();
                 definition.@class = "vulnerability";
@@ -83,7 +94,12 @@ namespace patchTuesVisualStudioToXML.GeneratorXML
                 {
                     Criteria criteriaMID = new Criteria();
                     criteriaMID.comment = "VS " + ProdIDYearPair.Value;
-                    criteriaMID.extendDefinition = generatorTags.GenerateTagExtendDefinitionVulnerability("Microsoft Visual Studio " + ProdIDYearPair.Value + " is installed", "oval:en.ovalxmlgen.win:def:" + lastDefID.ToString());
+                    string nameOfProduct = "Microsoft Visual Studio " + ProdIDYearPair.Value;
+                    criteriaMID.extendDefinition = generatorTags.GenerateTagExtendDefinitionVulnerability(nameOfProduct + " is installed", "oval:en.ovalxmlgen.win:def:" + lastDefrefID.ToString());
+                    //add ref doc (inventory)
+                    refInventoryDef.Add(generatorTags.GenerateTagDefinitionInventorySample(lastDefrefID, nameOfProduct, ProdIDYearPair.Value, lastTestID.ToString()));
+                    lastTestID++;
+                    lastDefrefID++;
                     Criteria criteriaBOT = new Criteria();
                     criteriaBOT.criterionsList = new List<Criterion>();
 
@@ -93,14 +109,32 @@ namespace patchTuesVisualStudioToXML.GeneratorXML
                         string upperVersion = remediation.FixedBuild;
                         if (remediation.URL.Contains(ProdIDYearPair.Value) || remediation.ProductIDList[0] == ProdIDYearPair.Key)
                         {
-                            criteriaBOT.criterionsList.Add(generatorTags.GenerateTagCriterionVulnerability(lowerVersion, upperVersion, lastTestID.ToString()));
-                            
+                            string comment = string.Format("Check if the version of Visual Studio is greater than or equal {0} and less than {1}", lowerVersion, upperVersion);
+                            criteriaBOT.criterionsList.Add(new Criterion("oval:en.ovalxmlgen.win:tst:" + lastTestID.ToString(), comment));
+                            //add test tag
+                            string objID = "oval:en.ovalxmlgen.win:obj:" + lastObjID.ToString();
+                            RegistryObject registryObject = CreateRegistryObjectWithRefs(lastVarID, nameOfProduct, objID);
+
+                            RegistryState registryState1 = new RegistryState(new ValueTAG("greater than or equal", "version", lowerVersion), "http://oval.mitre.org/XMLSchema/oval-definitions-5#windows", "oval:en.ovalxmlgen.win:ste:" + lastStateID, "1", "State holds if the version is greater than or equal " + lastStateID);
+                            lastStateID++;
+                            RegistryState registryState2 = new RegistryState(new ValueTAG("less than", "version", upperVersion), "http://oval.mitre.org/XMLSchema/oval-definitions-5#windows", "oval:en.ovalxmlgen.win:ste:" + lastStateID, "1", "State holds if the version is greater than or equal " + upperVersion);
+                            lastStateID++;
+
+
+                            List<State> states = new List<State>();
+
+                            RegistryTest registryTest = new RegistryTest("oval:en.ovalxmlgen.win:tst:" + lastTestID.ToString(), comment, states, objectTAG);
+
+                            resultOVALXML.tests.registryTest.Add(registryTest);
+
                             lastTestID++;
                         }
                     }
                     if (criteriaBOT.criterionsList.Count > 1)
                     {
                         criteriaMID.critersList = new List<Criteria>();
+                        criteriaBOT.comment = "Vulnerable versions";
+                        criteriaBOT.@operator = "OR";
                         criteriaMID.critersList.Add(criteriaBOT);
                     }
                     else
@@ -109,13 +143,16 @@ namespace patchTuesVisualStudioToXML.GeneratorXML
                         criteriaMID.criterionsList.Add(criteriaBOT.criterionsList[0]);
                     }
                     criteriaTOP.critersList.Add(criteriaMID);
+                    lastObjID++;
+                    lastVarID++;
                 }
 
 
                 definition.metadata = metadata;
                 definition.criteria = criteriaTOP;
                 resultOVALXML.definitions.definitionsList.Add(definition);
-
+                foreach (var def in refInventoryDef)
+                    resultOVALXML.definitions.definitionsList.Add(def);
                 lastDefID++;
             }
             
@@ -127,26 +164,63 @@ namespace patchTuesVisualStudioToXML.GeneratorXML
             generatorTags.WriteOVALXMLToFile(resultOVALXML);
         }
 
-        private List<Vulnerability> GetVulnerabilities(Cvrfdoc cvrfdoc)
+        private RegistryObject CreateRegistryObjectWithRefs(int lastVarID, string nameOfProduct, string objID)
+        {
+            ObjectTAG objectTAG = new ObjectTAG(objID);
+            KeyTAG keyTAG = new KeyTAG("oval:en.ovalxmlgen.win:var:" + lastVarID, "at least one");
+            RegistryObject registryObject = new RegistryObject(objID, "Registry key for " + nameOfProduct, new Behaviors("32_bit"), "HKEY_LOCAL_MACHINE", keyTAG, "DisplayVersion");
+
+
+
+
+
+            return registryObject;
+        }
+
+        private void CreateLocalVars(string id, string comment, string idRefObject, string version = "1", string datatype = "string")
+        {
+            var t = "Full key path of Microsoft Visual Studio 2022 from uninstall registry key";
+            var idRefObjectFull = "oval:en.ovalxmlgen.win:obj:" + idRefObject;
+            ObjectComponent objectComponent = new ObjectComponent("key", "oval:en.ovalxmlgen.win:obj:" + idRefObject);
+            LocalVariable localVariable = new LocalVariable("oval:en.ovalxmlgen.win:var:" + id, version, comment, datatype, objectComponent);
+            resultOVALXML.variables.localVariable.Add(localVariable);
+
+        }
+
+
+
+        private List<Vulnerability> GetVulnerabilities(Cvrfdoc cvrfdoc, string platformName = "Visual Studio")
         {
             List<Vulnerability> VulnerabListFiltredByProj = new List<Vulnerability>();
             foreach (Vulnerability vulnerability in cvrfdoc.VulnerabilitiesList)
             {
                 foreach (Note note in vulnerability.Notes.NotesList)
                 {
-                    if (note.Title == "Visual Studio") //TODO field for any platform/products
+                    if (note.Title == platformName) //TODO field for any platform/products
                     {
                         VulnerabListFiltredByProj.Add(vulnerability);
                         break;
                     }
                 }
             }
+
+            /*foreach (var vulnerability in from Vulnerability vulnerability in cvrfdoc.VulnerabilitiesList
+                                          from Note note in vulnerability.Notes.NotesList
+                                          where note.Title == platformName//TODO field for any platform/products
+                                          select vulnerability)
+            {
+                VulnerabListFiltredByProj.Add(vulnerability);
+                break;
+            }*/
+
             if (!VulnerabListFiltredByProj.Any())
             {
                 
             }
             return VulnerabListFiltredByProj;
-            
         }
+
+
+
     }
 }
